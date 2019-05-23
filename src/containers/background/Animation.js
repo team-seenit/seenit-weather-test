e4a566cbdb183b6e8b2812a208d09c92d824ae12
github username <github email address>
2019-05-23 17:31:09 +0100

    file structure updated

---
+++ src/containers/background/Animation.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Animation extends Component {
+   render() {
+      return(
+         <div id='background' className='section'>
+         </div>
+      )
+   }
+}
