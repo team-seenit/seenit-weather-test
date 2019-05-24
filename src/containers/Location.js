858b98f0476dcb1c03cb50167d20e4b13b2813ab
github username <github email address>
2019-05-24 17:01:28 +0100

    form inputs formatted

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -3,11 +3,24 @@ import React, { Component } from 'react';
 export default class Location extends Component {
    render() {
       return(
-         <div id='location' className='section'>
-            <form>
-               <input type='text' placeholder='CITY/TOWN' />
-               <input type='text' placeholder='COUNTRY' />
-            </form> 
+         <div id='location' className='section centre'>
+            <div className='content'>
+               <div className='intro'>
+                  <h2>How is the weather looking in your local area?</h2>
+                  <p>Find out here by simply inputting your address:</p>
+               </div>
+               <div className='search-query'>
+                  <div className='geoencode'>
+                  </div>
+                  <form>
+                     <input type='text' placeholder='CITY' />
+                     <input type='text' placeholder='COUNTRY' />
+                     <div className='btn'>
+                        <input type='submit' value='SEARCH' />
+                     </div>
+                  </form> 
+               </div>
+            </div>
          </div>
       )
    }
